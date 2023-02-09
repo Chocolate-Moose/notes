3c7e5b9b37d3e7e735258c6f8f761881c2bac108
Jessica Cheng <jessica.cheng.23@dartmouth.edu>
2023-02-09 15:11:46 -0500

    flip card button

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 function CardViewer(props) {
   // eslint-disable-next-line no-unused-vars
   const [index, setIndex] = useState(0);
+  const [displayFront, setDisplayFront] = useState(true);
 
   const displayCard = () => {
-    const card = props.cards[index];
+    const card = displayFront ? props.cards[index].front : props.cards[index].back;
 
     return (
       <div>
-        <p>{card.front}</p>
-        <p>{card.back}</p>
+        <p>{card}</p>
       </div>
     );
   };
@@ -19,6 +19,7 @@ function CardViewer(props) {
     <div>
       <h2>card viewer</h2>
       {displayCard()}
+      <button type="button" onClick={() => setDisplayFront(!displayFront)}>flip card</button>
       <hr />
       <button type="button" onClick={() => props.switchMode()}>switch to card editor</button>
     </div>
